391718bbbe0985d77b46500561ffdfeb9b1058c1
Mati Joseph <crucialniccur@gmail.com>
2025-05-20 08:09:51 +0300

    define youngest bear

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -27,8 +27,13 @@ select_oldest_bear_and_returns_name_and_age = """
         age
     FROM bears
     ORDER BY age DESC
-    LIMIT 1
+    LIMIT 1;
 """
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+        name,
+        age
+    FROM bears
+    ORDER BY age ASC
+    LIMIT 1;
 """
