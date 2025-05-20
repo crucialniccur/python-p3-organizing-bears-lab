40f50871e93d15d5d9ed9a777286fc4bc6ec3c52
Mati Joseph <crucialniccur@gmail.com>
2025-05-20 07:53:46 +0300

    define the INSERT INTO bears

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament,alive) VALUES
+ ('Mr. Chocolate', 22, 'M', 'Black','Sijui', 1),
+ ('Rowdy', 22, 'F', 'Colored','Yake tu ', 1),
+ ('Tabitha', 22, 'M', 'Black','Ile ', 0),
+ ('Sergeant Brown', 22, 'F', 'White','Fulani', 1),
+ ('Melissa', 22, 'M', 'Colored','Jamenni', 0),
+ ('Grinch', 22, 'F', 'Black','Salaale', 0),
+ ('Wendy', 22, 'M', 'White','Wahhh', 1),
+ (NULL, 22, 'F', 'Black','Aiyee', 0);
