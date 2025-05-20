5dd935eabd88d6d5a96f8e8fb0a1e7d1d8269400
Mati Joseph <crucialniccur@gmail.com>
2025-05-20 08:02:05 +0300

    define the selecting name and order in alphabetical order

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -7,7 +7,9 @@ select_all_female_bears_return_name_and_age = """
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    SELECT name
+    FROM bears
+    ORDER BY name ASC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
